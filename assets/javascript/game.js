1eb76c0e1d60f0e1cbdf8b1e19ebb709c2d3a19c
mahdi kalantari <mahdi kalantari>
2017-04-14 17:26:20 -0700

    final crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 	
-
+	//define variable
 	var rArray = [];
 	var rNum = 0 ;
 	var yourScore = 0;
@@ -8,11 +8,13 @@
 	var lose = 0;
 	var chosenNum;
 
+	
+	//Create a random number from 1-9
 	var Rndm = function(){
 
   		rNum = Math.floor(Math.random()*9 + 1);
 	};
-
+	//Reset function
 	var reset = function(){
 
 		yourScore = 0;
@@ -20,7 +22,7 @@
 		rArray = [];
 		reArray();
 	};
-
+	//randomly choose a number bigger than 10 and define unrepeated numbers for buttons
 	var reArray = function(){
 
 		chosenNum = Math.floor(Math.random() * 100 + 10);
@@ -44,39 +46,40 @@
 	$("#rno").html(chosenNum);
 	};
 
+	//this will check the result (win & lose)
 	var result = function(){
 		if (yourScore == chosenNum) {
 			win++;
 			$("#wns").html(win);
 			$("#yscore").empty();
-			reset();
+			$(".panel-body").append($("<img>").attr({"class":"wImg", "src":"assets/images/winner.jpg", "width":"200", "height":"200"}));
+			setTimeout(function(){ $('.wImg').fadeOut("fast", reset); }, 1500);
+			
 		}
 		if (yourScore > chosenNum) {
 			lose++;
 			$("#los").html(lose);
 			$("#yscore").empty();
-			reset();
+			$(".panel-body").append($("<img>").attr({"class":"lImg", "src":"assets/images/loser.jpg", "width":"200", "height":"200"}));
+			setTimeout(function(){ $('.lImg').fadeOut("fast", reset); }, 1500);
 		}
 	};
 
 
-
+	//on page load it will call reArray function to prepare everything for starting the game
 	$(document).ready(function() {
 
 
 		reArray();
 
 	});
-
+	//this onclick function will get the value from the image and will add it to yourscore
 	$(".image").on("click", function(){
 	
 			var imgNum = parseInt($(this).val());
 			yourScore = imgNum + yourScore;
 			$("#yscore").html(yourScore);
-			
-			
 			result();
-		
 		
 	});
 
